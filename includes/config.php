39d19e205cf2bce83c620c2c0f0d84bc934a5bb9
Tuğba <tugbanurayik@gmail.com>
2025-06-15 16:53:30 +0300

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+$port = 3307;
+$db = 'not_takas';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+try {
+    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Bağlantı hatası: " . $e->getMessage());
+}
+session_start();
+?>
